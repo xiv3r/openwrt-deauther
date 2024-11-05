201091fe8d9a197bc668dc685577a03388360462
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-05 09:34:02 +0800

    Update monitor_mode.sh

--- monitor_mode.sh
+++ monitor_mode.sh
@@ -3,20 +3,13 @@
 # Install aircrack-ng
 opkg install aircrack-ng openssh-sftp-server
 
-# Remove exit 0 from the top
-sed -i 's/exit 0//' /etc/rc.local
-
-# Copy cli to rc.local
-echo "echo 'mon0 monitor mode enable'" >> /etc/rc.local
-echo "iw phy phy0 interface add mon0 type monitor" >> /etc/rc.local
-echo "ip link set mon0 up" >> /etc/rc.local
-echo "exit 0" >> /etc/rc.local
+echo "echo 'mon0 monitor mode enable'" > monitor-mode.sh
+echo "iw phy phy0 interface add mon0 type monitor" >> monitor-mode.sh
+echo "ip link set mon0 up" >> monitor-mode.sh
 
 # Make Executable
-chmod +x /etc/rc.local
-cp /etc/rc.local /root/monitor_mode.sh
-sed -i 's/exit 0//' /root/monitor_mode.sh
+chmod +x monitor-mode.sh
 sh monitor_mode.sh
 
-# Launch airodump
+# Launch airodump-ng
 airodump-ng mon0
