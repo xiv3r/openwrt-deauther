e0819a4dd6ef60afd1e4143ebafafe1e44285787
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-05 10:14:45 +0800

    Update monitor_mode.sh

--- monitor_mode.sh
+++ monitor_mode.sh
@@ -1,17 +1,5 @@
-#!/bin/sh -e
-
-# Install aircrack-ng
-opkg install aircrack-ng openssh-sftp-server
-
-echo "
+#!/bin/sh
 echo 'mon0 monitor mode enable'
 iw phy phy0 interface add mon0 type monitor
 ip link set mon0 up
-" > monitor-mode.sh
-
-# Make Executable
-chmod +x monitor-mode.sh
-sh monitor_mode.sh
-
-# Launch airodump-ng
 airodump-ng mon0
