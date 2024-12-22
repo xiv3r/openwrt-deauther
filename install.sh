c31d1f0ca53e0bc6073661074602941a467278df
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-22 12:55:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ opkg update
 wget -O mdk4.ipk https://raw.githubusercontent.com/xiv3r/openwrt-deauther/refs/heads/main/mdk4_4.2-5_mipsel_24kc.ipk
 opkg install mdk4.ipk
 
-echo "
+cat >mon.sh << EOF
 iw phy phy0 interface add mon0 type monitor
 echo 'monitor mode mon0 enable'
 ip link set mon0 up
-" > mon.sh
+EOF
+#
 chmod +x mon.sh
+#
 sh mon.sh && mdk4 mon0 d
 
