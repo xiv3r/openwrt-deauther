911d5f559919e8a6c9efdeab5d89714d6a6c7bc3
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-04-09 18:20:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,11 @@ opkg update
 wget -O mdk4.ipk https://raw.githubusercontent.com/xiv3r/openwrt-deauther/refs/heads/main/mdk4_4.2-5_mipsel_24kc.ipk
 opkg install mdk4.ipk
 
-cat > wlan0mon.sh << EOF
+cat > wlan0mon << EOF
 iw phy phy0 interface add wlan0mon type monitor
-echo 'monitor mode enable in wlan0mon'
+echo "wlan0mon monitor mode is enabled"
 ip link set wlan0mon up
 EOF
-#
-chmod +x wlan0mon.sh
-#
-bash wlan0mon.sh && mdk4 wlan0mon d
+chmod +x wlan0mon
+sh wlan0mon && mdk4 wlan0mon d
 
